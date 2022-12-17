df2eb4723587df4c42d3e842245d2481cfa672f3
Josh Fleming <josh.fleming@mutations.ltd>
2022-12-17 15:42:00 -0800

    Move gtm-init to block TS and use prefs for update

--- privacy-toolset.php
+++ privacy-toolset.php
@@ -12,18 +12,19 @@
 // TODO setup plugin class
 // TODO standardize function names
 // TODO add noscript GTM code in body
+// TODO better version than current time
 
 function create_privacy_toolset_block() {
-  register_block_type(__DIR__ . '/block/build');
+	register_block_type(__DIR__ . '/block/build');
 }
 add_action('init', 'create_privacy_toolset_block');
 
-function enqueue_gtm() {
-  wp_enqueue_script(
-    'gtm-init',
-    __DIR__ . '/js/gtm-init.js?ver=' . time(),
-    array(),
-    false
-  );
-}
-add_action('init', 'enqueue_gtm');
+// function enqueue_gtm() {
+// 	wp_enqueue_script(
+// 		'gtm-init',
+// 		__DIR__ . '/block/build/gtm-init.js?ver=' . time(),
+// 		array(),
+// 		false
+// 	);
+// }
+// add_action('init', 'enqueue_gtm');
