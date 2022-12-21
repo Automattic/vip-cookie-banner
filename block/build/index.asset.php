3ef9b6a4b8c022308189d72ec0a65ccc9b973571
Josh Fleming <josh.fleming@mutations.ltd>
2022-12-21 10:15:59 -0800

    Troubleshoot analysis concat error Merge commit '8ece5220aadd48370932215b9c0414995b46e776' into jf/feature/cookie-privacy--prs-717

--- block/build/index.asset.php
+++ block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'd2e09c76328ee92746a6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '94aadf640bfe363e1938');
--- block/build/privacy-consent-banner.asset.php
+++ block/build/privacy-consent-banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'facd1add6fca12b96519');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9cbd5f0d6ec08d93c995');
