793a92b2714740859fb83f0da6fe567e232e998a
Josh Fleming <josh.fleming@mutations.ltd>
2022-12-21 17:09:51 -0800

    Build privacy plugin

---
+++ block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '94aadf640bfe363e1938');
---
+++ block/build/privacy-consent-banner.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'db8b3ed41d6ca75718f9');
